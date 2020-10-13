86d4c3a62c302243fab3d22883db5795fe8e7ea2
abhimanyu <thakurabhimanyu4@gmail.com>
2020-10-13 17:29:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        Product List
+      </main>
+      <footer>
+        All Right is Reserved.
+      </footer>
+    </div>
+  )
+}
+
+export default App
